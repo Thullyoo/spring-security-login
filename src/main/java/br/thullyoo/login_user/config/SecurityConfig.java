310597c8e6d9dbef429b1b3efa016fac48dacbf0
thullyo <thullyofilho@gmail.com>
2025-11-11 12:42:36 -0300

    feat: add SecurityFilter on SecurityFilterChain

--- src/main/java/br/thullyoo/login_user/config/SecurityConfig.java
+++ src/main/java/br/thullyoo/login_user/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package br.thullyoo.login_user.config;
 
+import jakarta.servlet.DispatcherType;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -11,19 +12,28 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    private final SecurityFilter securityFilter;
+
+    public SecurityConfig(SecurityFilter securityFilter) {
+        this.securityFilter = securityFilter;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth.
+                        dispatcherTypeMatchers(DispatcherType.ERROR).permitAll().
                         requestMatchers(HttpMethod.POST, "/user/register").permitAll().
                         requestMatchers(HttpMethod.GET, "/login").permitAll().
                         anyRequest().authenticated())
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
